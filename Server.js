98cb8759aa78ea74fd355141533cc0d8a5014f6a
MOHAMED HARATE <haratemo@gmail.com>
2021-03-29 09:13:38 +0200

    Rydder opp i koden.

--- Server.js
+++ Server.js
@@ -5,10 +5,7 @@ app.listen(2828, () => console.log('Listening for connections on port 2828....')
 app.use(express.static('public'));
 app.use(express.json())
 
-
-// Arrays
 users = [];
-
 chat_rooms = [];
 
 messages = [];
@@ -18,6 +15,8 @@ app.post('/api', (request, response) => {
 });
 
 // General functions
+
+
 
 function abort_if_not_exists(user_id){
 
@@ -30,20 +29,18 @@ function abort_if_exists(user_id){
     if(users.includes(user_id)){
         express.request.aborted = true; // Litt usikker på denne syntaksen. :')
     }
-
 }
 
-
-// Users
-
-// /api/users
+                                        // -------------- Users -------------------- //
 
 //get all. Return users array.
+
 app.get('/api/users', (req, res) => {
     res.send(users);
 });
 
-//add user.
+//add user. Return users array.
+
 app.post('/api/users', (req,res) => {
     const schema = {
         name: Joi.string().min(2).required()
@@ -63,7 +60,19 @@ app.post('/api/users', (req,res) => {
    res.send(users);
 });
 
-// /api/user
+// function to find the index of the room. (multidimensional)
+
+function findIndexOfRoom(arr,value){
+    let roomIndex;
+    for (let i = 0;i<arr.length;i++){
+        if (arr[i][0] === value){
+            roomIndex = i;
+            return roomIndex;
+        }
+    }
+}
+
+
 
 // get one user by given id
 
@@ -75,7 +84,8 @@ app.get('/api/user/:user_id', (req,res) => {
 
 });
 
-// Delete user
+// Delete user by given id
+
 app.delete('/api/user/:user_id', (req, res) =>{
     const user = users.find(c => c.user_id === parseInt(req.params.user_id));
     if (!user) res.status(404).send('The user with the given id was not found.');
@@ -86,39 +96,26 @@ app.delete('/api/user/:user_id', (req, res) =>{
     res.send(user);
 } );
 
-function getAllUsers(){
-    return users
-}
 
-function addOneUser(){
-
-}
 
 // /api/user/<user-id>
 
-// Restrictions:<user-id> is already registered.
 
-function getOneUser(user_id){
-    abort_if_not_exists(user_id);
-    return users[user_id];
-}
 
-function deleteOneUser(user_id){
-    abort_if_not_exists()
-    users = users.filter(function(user){
-        return user != user_id;
-    });
-}
 
-// Chat-Rooms
+
+                                            //  -- Chat-Rooms -- //
 
 // Get all
+
 app.get('/api/rooms', (req, res) => {
     res.send(chat_rooms);
 });
 
+//add one
 
 app.post('/api/rooms',(req, res) => {
+
     const schema = {
         name: Joi.string().min(2).required()
     };
@@ -126,13 +123,14 @@ app.post('/api/rooms',(req, res) => {
     const result = Joi.validate(req.body, schema);
 
     if (result.error){
-        res.status(400).send(result.error.details[0].message); // If the name is "Null" or less than 2 characters, the user will get an error with the details.
+        res.status(404).send(result.error.details[0].message); // If the name is "Null" or less than 2 characters, the user will get an error with the details.
         return;
     }
     const room = {
         room_id: chat_rooms.length + 1,
         name: req.body.name
     };
+
     chat_rooms.push(room);
     res.send(chat_rooms);
 });
@@ -145,14 +143,99 @@ app.get('/api/room/:room_id', (req,res) => {
 
 });
 
-
+//Get all users in the room
 app.get('/api/room/:room_id/users', (req, res) =>{
+
     const room = chat_rooms.find(c => c.room_id === parseInt(req.params.room_id));
     if (!room) res.status(404).send('The room with the given id was not found.');
 
+    const index = chat_rooms.indexOf(room);
+    res.send(chat_rooms[index]);
 });
 
-// /api/rooms
+// Restrictions:Only registered users can join
+
+
+app.post('/api/room/:room_id/users',(req,res) => {
+
+    const room = chat_rooms.find(c => c.room_id === parseInt(req.params.room_id));
+    if (!room) res.status(404).send('The room with the given id was not found.');
+
+    const schema = {
+        name: Joi.string().min(2).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if (result.error){
+        res.status(404).send(result.error.details[0].message); // If the name is "Null" or less than 2 characters, the user will get an error with the details.
+        return;
+    }
+
+    const user = users.find(c => c.user_id === parseInt(req.params.user_id));
+
+    const index = users.indexOf(room);
+
+    chat_rooms[index].push(user);
+    console.log(chat_rooms[0])
+
+    res.send(chat_rooms[index]);
+
+});
+
+
+
+
+
+                                            // -- Messages -- //
+
+// Restrictions:Only users in the room can get messages.
+
+// /api/room/<room-id>/messages
+
+
+app.get('/api/room/:room_id/messages', (req, res) => {
+});
+
+// /api/room/<room-id>/<user-id>/messages
+// Restrictions:
+//      ●Only users who have joined the room can get or addmessages.
+//      ●Only registered user-id's should be permitted as <user-id>
+
+
+app.get('/api/room/:room_id/:user_id/messages', (req, res) => {
+    const user_id = req.params.user_id;
+});
+
+
+app.post('/api/room/:room_id/:user_id/messages', (req, res) => {
+    const user_id = req.params.user_id;
+});
+
+
+/*
+
+// Restrictions:<user-id> is already registered.
+
+/*
+function getOneUser(user_id){
+    abort_if_not_exists(user_id);
+    return users[user_id];
+}
+
+function deleteOneUser(user_id){
+    abort_if_not_exists()
+    users = users.filter(function(user){
+        return user != user_id;
+    });
+}
+function getAllUsers(){
+    return users
+}
+
+function addOneUser(){
+
+}
 
 function getAllRooms(){
 
@@ -168,11 +251,11 @@ function getOneRoom(){
 
 }
 
-// Room users
+function addOneMessage(){
+}
 
-// /api/room/<room-id>/user
-// Restrictions:Only registered users can join
-
+function getAllMessages(){
+}
 function getAllRoomUsers(){
 
 }
@@ -181,31 +264,11 @@ function addOneRoomUser(){
 
 }
 
-// Messages
 
-// /api/room/<room-id>/messages
-// Restrictions:Only users in the room can get messages.
+ */
 
-function getAllMessages(){
-}
-app.get('/api/room/:room_id/messages', (req, res) => {
-});
 
-// /api/room/<room-id>/<user-id>/messages
-// Restrictions:
-//      ●Only users who have joined the room can get or addmessages.
-//      ●Only registered user-id's should be permitted as <user-id>
 
-function getAllUserMessages(){
-}
-app.get('/api/room/:room_id/:user_id/messages', (req, res) => {
-    const user_id = req.params.user_id;
-});
 
-function addOneMessage(){
-}
-app.post('/api/room/:room_id/:user_id/messages', (req, res) => {
-    const user_id = req.params.user_id;
-});
 
 
