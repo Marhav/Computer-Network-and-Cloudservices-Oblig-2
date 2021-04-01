5c2d94c4f58fc3e4b83647aa0c414092fba7337c
MOHAMED HARATE <haratemo@gmail.com>
2021-04-01 13:17:50 +0200

    Jobber med index.html og Server.js. Har fått til å registrere en ny bruker med passord fra html. Har også gjort noen endringer på metodene i Server.js slik at når en bruker registrer seg, får hen en melding om at regitreringen er fullført. Har også konvertert fra tekst i input-feltene i index.html til json-objekter. Har osgå lastet ned body-parser og string-builder.

---
+++ public/test.js
@@ -0,0 +1,22 @@
+
+arr1 = [[]];
+arr1[0].push("h")
+arr1[0].push(2)
+
+console.log(arr1[0])
+
+arr1.indexOf("h")
+
+const arr = [["h",1],["d",2],["en",3]];
+
+
+
+console.log(arr.length);
+
+const d = arr.map(x => x.map(a => a[0])).flat(2)
+
+
+
+
+
+
