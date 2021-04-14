44dd2e594443a628e9be11f6890045d0b2adb713
marhav <marhav95@gmail.com>
2021-04-14 13:34:07 +0200

    Fikset app.route('/api/room/:room_id/:username/messages') sin .get metode.
    Denne returnerte før alle meldinger i et rom. Nå returnerer den kun meldingene til bruker, as intended.

--- Server.js
+++ Server.js
@@ -345,7 +345,15 @@ app.route('/api/room/:room_id/:username/messages')
     const user = users.find(c => c.username === req.params.username);
     if (!user) return  res.status(404).send('The user with the given username was not found.');
 
-        res.status(200).send(foramterMsgs(room.messages)); // NB! MÅ IKKE RETURNERE ALLE MELDINGER I ROMMET!
+    const user_messages = [];
+
+    for (let i = 0; i < room.messages.length; i++){
+        if(room.messages[i].sender === user.username){
+            user_messages.push(room.messages[i].message)
+        }
+    }
+
+    res.status(200).send(user_messages); // NB! MÅ IKKE RETURNERE ALLE MELDINGER I ROMMET!
     })
     //Add message
     .post((req, res) => {
